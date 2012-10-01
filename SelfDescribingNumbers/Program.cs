5eee8815b6b6bfdfc2fa089e53eece9e4a1d83b5
Joe Letizia <joe.letizia@gmail.com>
2012-10-01 17:19:11 -0400

    Ran ReSharper code cleanup. It didn't change much apparently...

--- SelfDescribingNumbers/Program.cs
+++ SelfDescribingNumbers/Program.cs
@@ -1,22 +1,21 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
 using SelfDescribingNumbers;
 
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
         using (StreamReader reader = File.OpenText(args[0]))
-        while (!reader.EndOfStream)
-        {
-            string line = reader.ReadLine();
-            if (null == line)
-                continue;
-            // do something with line
-            ISelfDescribingFinder finder = new SimpleSelfDescribingNumberfinder();
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+                // do something with line
+                ISelfDescribingFinder finder = new SimpleSelfDescribingNumberfinder();
 
-            Console.WriteLine(finder.Test(int.Parse(line)) ? 1 : 0);
-        }
+                Console.WriteLine(finder.Test(ulong.Parse(line)) ? 1 : 0);
+            }
     }
 }
